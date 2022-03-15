83b5e6241f7648f2e093896e50f044949d276944
jonatas falkaniere <jonatasfalkaniere@gmail.com>
2022-03-15 08:26:50 -0300

    chore: server and front-end'

---
+++ server/src/schema.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        tracksForHome: [Track!]!
+        spaceCats: [SpaceCat]
+    }
+
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int      
+    }
+
+    type Author { 
+        id: ID!
+        name: String!
+        photo: String
+    }
+
+    type SpaceCat {
+    id: ID!
+    name: String!
+    age: Int
+    missions: [Mission]
+    }
+
+    type Mission {
+        id: ID!
+        name: String!
+        description: String!
+    }
+`; 
+
+module.exports = typeDefs;
